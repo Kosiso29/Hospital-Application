da00acb14547ae468aef427e28fcac4a6a19019e
kosisochukwu <kafoenyi@gmail.com>
2021-06-25 01:48:12 +0100

    Final deployment prep

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-require('dotenv').config();
+// require('dotenv').config();
+require("dotenv").config({ path: "./config.env" });
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,6 +27,17 @@ const patientsRouter = require('./routes/patients')
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
 app.use('/patients', patientsRouter);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '/projectreact/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'projectreact', 'build', 'index.html'));
+    })
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running');
+    });
+}
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
